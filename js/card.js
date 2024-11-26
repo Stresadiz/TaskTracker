b24ae3bac954bc1a267585fbb642dd15ac15aad9
Sebastian Luna <eliaslunaslo@gmail.com>
2024-11-26 00:02:19 -0300

    Proyecto de task tracker con retorno de JSON

--- js/card.js
+++ js/card.js
@@ -3,32 +3,126 @@ var cardDefault = document.getElementById("cardDefault");
 const addNewCardBtn = document.getElementById("addNewCard");
 
 const maxCards = 20;
+const maxLenghtTitle = 20;
+const maxLenghtTextCard = 140;
+var title = "";
+var text = "";
 
-/* Funcionalidad de agregar cards */
+/* Objeto constructor de JSON*/
+let arrayDeCards = new Array();
+let arrayDeCardsParallel = new Array();
+let objJson;
+    
+let numCard = 0;
 
 addNewCardBtn.addEventListener("click", e =>{
-    var cardNew = document.createElement("div");
-    cardNew.className = "card";
-    cardNew.innerHTML = `
-                <div class="card-body">
-                    <h2 class="card-title">Card Title</h2>  
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis esse aliquid hic.</p>
-                    <button class="btn btn-danger">
-                        Delete
-                    </button>
-                    <div class="card-footer">
-                        <label for="Complete">Completada:</label><input type="checkbox" name="Complete" id="complete">
-                    </div> 
-                </div>
-    `
+    
+    while (title == "" || title.length > maxLenghtTitle) {
+        title = prompt("Ingrese el titulo aqui:");
+        if (title.length > maxLenghtTitle) {
+            alert("El texto a ingresar no puede superar los " + maxLenghtTitle + " caracteres")
+        }
+    }
+    while (text == "" || text.length > maxLenghtTextCard) {
+        text = prompt("Ingrese el texto aqui:");
+        if (title.length > maxLenghtTitle) {
+            alert("El texto a ingresar no puede superar los " + maxLenghtTextCard + " caracteres")
+        }
+    }
+
     const cardNodes = grid.querySelectorAll('div.card');
     
     if (cardNodes.length < maxCards) {
-        grid.removeChild(cardDefault);
-        grid.appendChild(cardNew);
-        grid.appendChild(cardDefault);
+        
+        let objCard = {titleCard:title, textCard:title, complete:false, id:numCard}
+        arrayDeCards[numCard] = objCard;
+        
+        
+        objJson = JSON.stringify(arrayDeCards, null, 2);
+        document.getElementById("textJson").value = objJson;
+
+        renderCards(arrayDeCards);
+
+        numCard++;
+
     } else{
         alert("no puedes agregar mas tasks");
     }
-    
+
+    title = "";
+    text = "";
+
+
 })
+
+function deleteInBtns() {
+    var btnsDelete = grid.querySelectorAll(".btn-danger");
+
+    btnsDelete.forEach(btn => {
+        btn.addEventListener("click", e =>{
+            grid.removeChild(btn.parentNode.parentNode);
+        })
+    });
+}
+
+function assignCheckboxEvents() {
+    let checkboxComplete = grid.querySelectorAll("input[name='Complete']");
+    checkboxComplete.forEach((input) => {
+        input.addEventListener('change', () => {
+            completesChecks(checkboxComplete);
+            objJson = JSON.stringify(arrayDeCards, null, 2);
+            document.getElementById("textJson").value = objJson;
+        });
+    });
+}
+
+function completesChecks(array) {
+    array.forEach((input, index) =>{
+        arrayDeCards[index].complete = input.checked;
+    })
+}
+
+function renderCards(array) {
+        grid.innerHTML = "";
+
+        array.forEach(card => {
+            let checked = card.complete ? "checked" : ""
+            var cardNew = document.createElement("div");
+            cardNew.className = "card";
+            cardNew.innerHTML = `
+            <div class="card-body">
+            <h2 class="card-title">${card.titleCard}</h2>  
+            <p class="card-text">${card.textCard}</p>
+            <button class="btn btn-danger">
+            Delete
+            </button>
+            <div class="card-footer">
+            <label for="Complete">Completada:</label><input type="checkbox" name="Complete" id="complete${card.id}" ${checked}>
+            </div> 
+            </div>
+            `
+            grid.appendChild(cardNew);
+        });
+
+        grid.appendChild(cardDefault); 
+
+        deleteInBtns();
+        assignCheckboxEvents();
+}
+
+function showCards(state) {
+    if (state === "completes") {
+        arrayDeCardsParallel = arrayDeCards.filter(card => card.complete);
+    } else if (state === "noCompletes") {
+        arrayDeCardsParallel = arrayDeCards.filter(card => !card.complete);
+    } else if (state === "all") {
+        arrayDeCardsParallel = arrayDeCards
+    }
+
+    renderCards(arrayDeCardsParallel);
+}
+
+function validateValue() {
+    var selectValue = document.getElementById("cardFilter").value;
+    showCards(selectValue)
+}
